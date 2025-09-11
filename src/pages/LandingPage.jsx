f5e9a42b42a20a610f111a9bf1812634dea1c5c5
sarkar niloy <sarkarniloy102@gmail.com>
2025-09-11 15:50:59 +0600

    stat in left section ..... completed

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -134,6 +134,27 @@ const LandingPage = () => {
 
                             </div>
 
+                            {/* stats grid */}
+                            <div className={landingPageStyles.statsContainer}>
+                                {
+                                    [
+                                        { value: '50K+', label: 'Resumes Created', gradient: 'from-violet-600 to-fuchsia-600' },
+                                        { value: '4.9★', label: 'User Rating', gradient: 'from-orange-500 to-red-500' },
+                                        { value: '5 Min', label: 'Build Time', gradient: 'from-emerald-500 to-teal-500' }
+                                    ].map((stat, idx) => (
+                                        <div className={landingPageStyles.statItem} key={idx}>
+                                            <div className={`${landingPageStyles.statNumber} ${stat.gradient}`}>
+                                                {stat.value}
+                                            </div>
+                                            <div className={landingPageStyles.statLabel}>
+                                                {stat.label}
+                                            </div>
+                                        </div>
+                                    ))
+                                }
+
+                            </div>
+
                         </div>
                     </div>
                 </section>
