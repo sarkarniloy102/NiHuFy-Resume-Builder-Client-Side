d441689fbad68311591ef4984582ee9b88bda884
sarkar niloy <sarkarniloy102@gmail.com>
2025-09-20 00:59:52 +0600

    implementing createresumeform

--- src/components/CreateResumeForm.jsx
+++ src/components/CreateResumeForm.jsx
@@ -2,8 +2,12 @@
 
 const CreateResumeForm = () => {
     return (
-        <div>
-            resume
+        <div className="w-full max-w-md p-8 bg-white rounded-2xl border border-gray-100 shadow-lg">
+            <h3 className="text-2xl font-bold text-gray-900 mb-2">Create New Resume</h3>
+            <p className="text-gray-600 mb-8">
+                Give your resume a title to get started.You can change it later
+            </p>
+            
         </div>
     );
 };
